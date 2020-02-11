fabe65fa72487d6e2437cb2de6b4269f3cd26518
wira <wirasetiawan29@gmail.com>
2020-02-11 14:49:46 +0700

    init project

---
+++ LBWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LBWeather
+//
+//  Created by wira on 2/11/20.
+//  Copyright © 2020 Wira Setiawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
